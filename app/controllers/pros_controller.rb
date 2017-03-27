e7c2fad0d6a8c781dbc6078c2825074f2cceb5fd
root <root@192.168.1.3>
2017-03-28 01:22:46 +0200

    Add product views

--- app/controllers/pros_controller.rb
+++ app/controllers/pros_controller.rb
@@ -1,14 +1,24 @@
 class ProsController < ApplicationController
+  before_action :find_pro , only:[:show ,:edit ,:update ,:destroy]
   def index
+    @pros =Pro.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @pro =Pro.new
   end
 
   def create
+    @pro =Pro.new(pro_params)
+
+    if @pro.save
+      redirect_to @pro
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -23,8 +33,11 @@ class ProsController < ApplicationController
   private
 
   def find_pro
+    @pro= Pro.find(params[:id])
+
   end
 
   def pro_params
+    params.require(:pro).permit(:name, :price, :category, :photo)
   end
 end
